5615c6eabc82e3d015075eb31e95b62f450b3ff0
DweightFuentes <dwieghtpro@gmail.com>
2023-10-07 23:32:42 +0100

    Fuentes, Abang, Caballero Commit

---
+++ ASI.Basecode.WebApp/Controllers/DwieghtController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASI.Basecode.WebApp.Controllers
+{
+    public class DwieghtController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
